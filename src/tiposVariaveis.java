dd7062b6bb344d640ead6ac0ed7f98f9138a9d1c
Camila <camilasouza.psicologia@gmail.com>
2023-03-25 19:59:07 -0300

    Operadores

---
+++ src/tiposVariaveis.java
@@ -0,0 +1,22 @@
+public class tiposVariaveis {
+    public static void main(String[] args) throws Exception {
+       // estudar: tipos primitivos
+       // a classe String que representa texto na aplicação
+
+       String meuNome = "Camila Gonçalves Souza";
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+
+
+    }
+}
+
